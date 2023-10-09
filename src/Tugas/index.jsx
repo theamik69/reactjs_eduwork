faa7fae0a9c2446bf9d2d3bc06e49527d4da060e
DESKTOP-HR98PIA\Siti Hajar Soleha <amishbah12@gmail.com>
2023-10-09 12:34:10 +0700

    update index.jsx

---
+++ src/Tugas/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ClassComponent from "./Komponen/ClassComponent";
+
+export default class Komponen extends React.Component {
+    render() {
+      return (
+        <div>
+          <ClassComponent />
+        </div>
+      );
+    }
+  }
