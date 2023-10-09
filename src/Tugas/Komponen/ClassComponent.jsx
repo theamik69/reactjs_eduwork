bde299d42c95df09cae867a7b7834cee8173e834
DESKTOP-HR98PIA\Siti Hajar Soleha <amishbah12@gmail.com>
2023-10-09 12:27:38 +0700

    add ClassComponent

---
+++ src/Tugas/Komponen/ClassComponent.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+class ClassComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          count: 0
+        };
+      }
+
+      incrementCount = () => {
+        this.setState({ count: this.state.count + 1 });
+      }
+
+      decrementCount = () => {
+        this.setState({ count: this.state.count - 1 });
+      }
+
+      render() {
+        return (
+          <div>
+            <h1>Counter</h1>
+            <p>Count: {this.state.count}</p>
+            <button onClick={this.incrementCount}>Increment</button>
+            <button onClick={this.decrementCount}>Decrement</button>
+          </div>
+        );
+      }
+}
+
+export default ClassComponent;
