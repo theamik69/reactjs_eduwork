a5ae495754f1610c349f19d72782125814d1aa7b
DESKTOP-HR98PIA\Siti Hajar Soleha <amishbah12@gmail.com>
2023-10-09 12:06:28 +0700

    fix index.jsx

--- src/Materi/Tugas/Komponen/index.jsx
+++ src/Materi/Tugas/Komponen/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ClassComponent from "./Pembahasan/ClassComponent";
-import FungtionalKomponent from "./Pembahasan/FungtionalKomponent";
+import FungtionalKomponent from "./Pembahasan/FungtionalComponent";
 
 export default class Komponen extends React.Component {
   render() {
